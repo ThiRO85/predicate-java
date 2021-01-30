dd71ca2c629c1cda1081cafcece782027cc74a76
Thiago Rodrigues <thgr85@gmail.com>
2021-01-30 15:15:46 -0200

    Implementacao da interface

---
+++ src/model/util/ProductPredicate.java
@@ -0,0 +1,13 @@
+package model.util;
+
+import model.entities.Product;
+
+import java.util.function.Predicate;
+
+public class ProductPredicate implements Predicate<Product> {
+
+    @Override
+    public boolean test(Product p) {
+        return p.getPrice() >= 100.00;
+    }
+}
