e6d5ca790706253ec2ee5a8c70de470a28f2cbf0
Thiago Rodrigues <thgr85@gmail.com>
2021-01-30 15:53:22 -0200

    Expressao lambda declarada

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,6 +4,7 @@ import model.entities.Product;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Main {
 
@@ -16,7 +17,9 @@ public class Main {
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
 
-        list.removeIf(Product::nonStaticProductPredicate);
+        Predicate<Product> pred = p -> p.getPrice() >= 100.00;
+
+        list.removeIf(pred);
 
         for (Product p : list) {
             System.out.println(p);
